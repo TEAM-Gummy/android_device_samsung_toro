bdc575dda45eff4b7f8fa6b4a92569c27746182e
hedwig34 <hedwig34@gmail.com>
2013-10-15 15:28:58 -0400

    Gummy Fied
    Change-Id: I02d17ec60f1efb5b1a71a2fcb85e467601355a1a

--- tg.mk
+++ tg.mk
@@ -6,17 +6,17 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toro
-PRODUCT_NAME := cm_toro
+PRODUCT_NAME := tg_toro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
